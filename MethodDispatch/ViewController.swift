862a6303889ec823d1537a948ad85e4316ef1d3f
Bulat <khabibullin23@mail.ru>
2021-09-20 18:43:36 +0300

    Initial Commit

---
+++ MethodDispatch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MethodDispatch
+//
+//  Created by Булат Хабибуллин on 20.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
